b9a281b2725fa00dedc5f4197e3ecd2f9195494d
unknown <contatopaulohn@gmail.com>
2024-04-01 23:43:24 -0300

    attsosrecife

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,16 @@ function About() {
     return (
         <div>
             <h1>About</h1>
-            <p> teste</p>
+            <p> O SOS Recife é um site dedicado à exploração de soluções inovadoras baseadas em tecnologia para reduzir os
+             riscos de incidentes com tubarões na orla do Recife. Nosso objetivo é fornecer informações atualizadas, análises especializadas e 
+             recursos práticos para aumentar a segurança dos banhistas, surfistas e entusiastas do mar na região. Por meio de artigos, entrevistas, 
+             vídeos e infográficos, buscamos conscientizar o público sobre os desafios enfrentados e promover o diálogo sobre medidas preventivas e 
+             intervenções eficazes. Com uma abordagem colaborativa e orientada para a inovação, o SOS Recife se posiciona como um catalisador para o 
+             desenvolvimento de estratégias sustentáveis que protejam tanto os seres humanos quanto o ecossistema marinho local. Junte-se a nós nesta 
+             missão vital de transformar a relação entre humanos e tubarões em uma coexistência harmoniosa e segura.</p>
+           
+            <p> Grupo: Paulo Henrique Nascimento de Carvalho e Thiago</p>
+            <p> https://github.com/paulohenriquenc/sosrecifee </p>
 
 
 
